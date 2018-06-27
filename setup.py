d238e0ed4f0adf529a330ae01ce1132b19e45ba4
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-06-27 12:25:29 -0700

    0.5.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info < (3, 5):
 
 setup(
     name='ament_tools',
-    version='0.4.0',
+    version='0.5.0',
     packages=find_packages(exclude=['test', 'test.*']),
     install_requires=['ament-package', 'osrf_pycommon'],
     zip_safe=False,
